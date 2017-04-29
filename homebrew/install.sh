9c0b1c15b9253c365d61f0d3d4d3791ca8ada6c4
Paul Bouzakis <github@paulbouzakis.com>
2017-04-29 11:34:16 -0400

    Add support for homebrew.

---
+++ homebrew/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+brew tap homebrew/bundle
+
+brew bundle --file=homebrew/Brewfile
+
+brew link node
+brew install yarn
